ecdd0805f77fd8308ea08c003c758ba211a210aa
Adel <adel@abtasty.com>
2020-02-19 11:36:40 +0100

    add test unit

--- FlagShip/FlagShipTests/FlagshipTests.swift
+++ FlagShip/FlagShipTests/FlagshipTests.swift
@@ -1,14 +1,15 @@
 //
-//  FlagshipTest.swift
-//  FlagShipTests
+//  FlagshipTests.swift
+//  FlagshipTests
 //
-//  Created by Adel on 18/02/2020.
+//  Created by Adel on 19/02/2020.
 //  Copyright © 2020 FlagShip. All rights reserved.
 //
 
 import XCTest
+@testable import Flagship
 
-class FlagshipTest: XCTestCase {
+class FlagshipTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -25,9 +26,20 @@ class FlagshipTest: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    
+    func testStartFlagship(){
+        
+        
+        Flagship.sharedInstance.startFlagShipWithMode(environmentId: "", "", .BUCKETING) { (result) in
+            
+            
+        }
+        
     }
 
 }
