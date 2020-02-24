4af6237345b0c3b2d1713cc1f4342d8d0dd3a53d
Adel <adel@abtasty.com>
2020-02-24 14:37:59 +0100

    rename package swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "FlagShip",
+    name: "Flagship",
     
     platforms: [
         .iOS(.v10),
@@ -13,12 +13,12 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "FlagShip",
-            targets: ["FlagShip"]),
+            name: "Flagship",
+            targets: ["Flagship"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "FlagShip",path:"Source")
+        .target(name: "Flagship",path:"Source")
     ]
 )
