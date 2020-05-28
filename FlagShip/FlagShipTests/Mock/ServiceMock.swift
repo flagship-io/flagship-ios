d669d8962f8ed1b344684864c3b20e49729ea60e
Adel <adel@abtasty.com>
2020-05-29 01:44:20 +0200

    add unit test

--- FlagShip/FlagShipTests/Mock/ServiceMock.swift
+++ FlagShip/FlagShipTests/Mock/ServiceMock.swift
@@ -15,8 +15,25 @@ class ServiceMock:ABService {
     
     override func getCampaigns(_ currentContext: Dictionary<String, Any>, onGetCampaign: @escaping (FSCampaigns?, FlagshipError?) -> Void) {
         
-        
-        onGetCampaign(nil,nil)
+        /// read the data from the file and fill the campaigns
+        do {
+            
+            let testBundle = Bundle(for: type(of: self))
+
+
+
+            guard let path = testBundle.url(forResource: "bucketMock", withExtension: "json") else { return  }
+            
+            let data = try Data(contentsOf: path, options:.alwaysMapped)
+            
+            let scriptObject = try JSONDecoder().decode(FSCampaigns.self, from: data)
+            
+            onGetCampaign(scriptObject,nil)
+        }catch{
+            
+            print("error")
+        }
+       
     }
     
     
