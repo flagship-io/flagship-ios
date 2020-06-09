736ed92c1260706e327e169ed20eb89a687bc69f
Adel <adel@abtasty.com>
2020-06-09 11:32:19 +0200

    clean

---
+++ Example/Pods/FlagShip/FlagShip/Classes/Models/FSCampaigns+Modifications.swift
@@ -0,0 +1,16 @@
+//
+//  FSCampaigns+Modifications.swift
+//  Flagship
+//
+//  Created by Adel on 06/08/2019.
+//
+
+import Foundation
+
+
+extension FSCampaigns{
+    
+    
+    //// have to be review
+   
+}
