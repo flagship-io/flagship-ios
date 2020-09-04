72e6370f395864eff9ae401df2987d6110b52e19
ABTastyAdel <adel@abtasty.com>
2020-09-04 14:42:02 +0200

    Update build_universal_fmk.sh

--- .github/scripts/build_universal_fmk.sh
+++ .github/scripts/build_universal_fmk.sh
@@ -9,9 +9,9 @@ UNIVERSAL_OUTPUTFOLDER=$PWD/build/${CONFIGURATION}-universal
 mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
 
 # Step 1. Build Device and Simulator versions
-#xcodebuild -target "Flagship" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk iphoneos  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -target "Flagship" ONLY_ACTIVE_ARCH=NO -configuration ${CONFIGURATION} -sdk "iphoneos"  BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
-xcodebuild -target "Flagship" -configuration ${CONFIGURATION} -sdk iphonesimulator -arch x86_64 BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
+xcodebuild -target "Flagship" -configuration ${CONFIGURATION} -sdk "iphonesimulator" -arch x86_64 BUILD_DIR="${BUILD_DIR}" BUILD_ROOT="${BUILD_ROOT}" clean build
 
 # Step 2. Copy the framework structure to the universal folder
 cp -R "${BUILD_DIR}/${CONFIGURATION}-iphoneos/Flagship.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
