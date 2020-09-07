6ae8fcc672a035f34ed56ad41f00e1d77bcb0a8f
ABTastyAdel <adel@abtasty.com>
2020-09-08 01:43:01 +0200

    Update build_universal_fmk.sh
    b

--- .github/scripts/build_universal_fmk.sh
+++ .github/scripts/build_universal_fmk.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 # Type a script or drag a script file from your workspace to insert its path.
 UNIVERSAL_OUTPUTFOLDER=$PWD/build/universal
 
-mkdir -p  $PWD/build
+ 
 
 # make sure the output directory exists
 mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
@@ -18,7 +18,7 @@ echo $BUILD_DIR
 echo trace --------------
 
 # Step 2. Copy the framework structure to the universal folder
-cp -R "$PWD/build/Debug-iphoneos/Flagship.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+cp -R "$PWD/FlagShip/build/Debug-iphoneos/Flagship.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
 
 # Step 3. Create universal binary file using lipo and place the combined executable in the copied framework directory
 lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/Flagship.framework/Flagship" "$PWD/build/Debug-iphonesimulator/Flagship.framework/Flagship" "$PWD/build/Debug-iphoneos/Flagship.framework/Flagship"
