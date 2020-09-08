2f8d70a867a1eb99496bca5582d1619c7f6ed088
ABTastyAdel <adel@abtasty.com>
2020-09-08 18:32:48 +0200

    Update build_universal_fmk.sh
    v

--- .github/scripts/build_universal_fmk.sh
+++ .github/scripts/build_universal_fmk.sh
@@ -31,6 +31,7 @@ lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/Flagship.framework/Flagship" "${
 
 cp -r "${BUILD_FOLDER}/Debug-iphonesimulator/Flagship.framework/Modules/Flagship.swiftmodule/" "${UNIVERSAL_OUTPUTFOLDER}/Flagship.framework/Modules/Flagship.swiftmodule"
 
+mkdir -p path/to/artifact
 Run cp "/Users/runner/work/flagship-ios/build/universal/Flagship.framework" "path/to/artifact/Flagship.framework"
 
 echo ---- Done -------
