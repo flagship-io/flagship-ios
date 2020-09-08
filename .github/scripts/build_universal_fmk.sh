4a77fa21e104ebf8a8b0c6f3338bd809782aad69
ABTastyAdel <adel@abtasty.com>
2020-09-08 10:41:41 +0200

    Update build_universal_fmk.sh

--- .github/scripts/build_universal_fmk.sh
+++ .github/scripts/build_universal_fmk.sh
@@ -3,26 +3,41 @@ set -eo pipefail
 # Type a script or drag a script file from your workspace to insert its path.
 UNIVERSAL_OUTPUTFOLDER=$PWD/build/universal
 
+BUILD_DIR="build"
 
-echo ${BUILD_ROOT}
+mkdir -p ${BUILD_DIR}
+
  
 
 # make sure the output directory exists
 mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
 
-# Step 1. Build Device and Simulator versions
-xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphoneos ONLY_ACTIVE_ARCH=NO  BUILD_DIR="build" BUILD_ROOT="${BUILD_ROOT}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+
+
+echo --------------
+echo ${BUILD_ROOT}
+echo --------------
+echo ${BUILD_DIR}
+echo --------------
+
  
 
-xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphonesimulator -arch x86_64 ONLY_ACTIVE_ARCH=NO BUILD_DIR="build" BUILD_ROOT="${BUILD_ROOT}"
 
-echo $BUILD_DIR
-echo trace --------------
+
+
+
+# Step 1. Build Device and Simulator versions
+#xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphoneos ONLY_ACTIVE_ARCH=NO  BUILD_DIR="build" BUILD_ROOT="${BUILD_ROOT}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+ 
+
+#xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphonesimulator -arch x86_64 ONLY_ACTIVE_ARCH=NO BUILD_DIR="build" BUILD_ROOT="${BUILD_ROOT}"
+
+ 
 
 # Step 2. Copy the framework structure to the universal folder
 cp -R "$PWD/FlagShip/build/Debug-iphoneos/Flagship.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
 
 # Step 3. Create universal binary file using lipo and place the combined executable in the copied framework directory
-lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/Flagship.framework/Flagship" "$PWD/build/Debug-iphonesimulator/Flagship.framework/Flagship" "$PWD/build/Debug-iphoneos/Flagship.framework/Flagship"
+#lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/Flagship.framework/Flagship" "$PWD/build/Debug-iphonesimulator/Flagship.framework/Flagship" "$PWD/build/Debug-iphoneos/Flagship.framework/Flagship"
 
 #cp -r "${BUILD_DIR}/Debug-iphonesimulator/Flagship.framework/Modules/Flagship.swiftmodule/" "${UNIVERSAL_OUTPUTFOLDER}/Flagship.framework/Modules/Flagship.swiftmodule"
