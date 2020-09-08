fe25192b6c94bfb588ed0f6523913b930eb711b8
ABTastyAdel <adel@abtasty.com>
2020-09-08 10:56:53 +0200

    Update build_universal_fmk.sh
    s

--- .github/scripts/build_universal_fmk.sh
+++ .github/scripts/build_universal_fmk.sh
@@ -2,12 +2,11 @@ set -eo pipefail
 
 # Type a script or drag a script file from your workspace to insert its path.
 UNIVERSAL_OUTPUTFOLDER=$PWD/build/universal
+BUILD_FOLDER = $PWD/build
 
 BUILD_DIR="build"
 
-mkdir -p ${BUILD_DIR}
-
- 
+mkdir -p ${BUILD_FOLDER}
 
 # make sure the output directory exists
 mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
@@ -17,7 +16,7 @@ mkdir -p "${UNIVERSAL_OUTPUTFOLDER}"
 echo --------------
 echo ${BUILD_ROOT}
 echo --------------
-echo ${BUILD_DIR}
+echo ${BUILD_FOLDER}
 echo --------------
 echo --------------
 echo ${PWD}
@@ -29,15 +28,15 @@ echo --------------
 
 
 # Step 1. Build Device and Simulator versions
-#xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphoneos ONLY_ACTIVE_ARCH=NO  BUILD_DIR="build" BUILD_ROOT="${BUILD_ROOT}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphoneos ONLY_ACTIVE_ARCH=NO  BUILD_DIR=${BUILD_FOLDER} BUILD_ROOT="${BUILD_ROOT}" CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
  
 
-#xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphonesimulator -arch x86_64 ONLY_ACTIVE_ARCH=NO BUILD_DIR="build" BUILD_ROOT="${BUILD_ROOT}"
+xcodebuild  build -project Flagship/Flagship.xcodeproj -scheme "Flagship"  -sdk iphonesimulator -arch x86_64 ONLY_ACTIVE_ARCH=NO BUILD_DIR=${BUILD_FOLDER} BUILD_ROOT="${BUILD_ROOT}"
 
  
 
 # Step 2. Copy the framework structure to the universal folder
-cp -R "FlagShip/build/Debug-iphoneos/Flagship.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
+cp -R ${BUILD_FOLDER}/Debug-iphoneos/Flagship.framework" "${UNIVERSAL_OUTPUTFOLDER}/"
 
 # Step 3. Create universal binary file using lipo and place the combined executable in the copied framework directory
 #lipo -create -output "${UNIVERSAL_OUTPUTFOLDER}/Flagship.framework/Flagship" "$PWD/build/Debug-iphonesimulator/Flagship.framework/Flagship" "$PWD/build/Debug-iphoneos/Flagship.framework/Flagship"
