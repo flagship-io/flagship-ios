f265d66f681e690f25b88e8ee52cfb74c4260d7a
Adel <adel@abtasty.com>
2020-11-04 14:16:47 +0100

    clean code

--- iOS Example/iOS Example/FSLoginViewController.swift
+++ iOS Example/iOS Example/FSLoginViewController.swift
@@ -67,23 +67,23 @@ class FSLoginViewController: UIViewController, UITextFieldDelegate {
     
     /// Start Flagship
     
-    Flagship.sharedInstance.start(envId:"bkk9glocmjcg0vtmdlng", apiKey: "DxAcxlnRB9yFBZYtLDue1q01dcXZCw6aM49CQB23", visitorId: nil, config: FSConfig(.BUCKETING, timeout:0.4)) { (result) in
+    Flagship.sharedInstance.start(envId:"your envId", apiKey: "your apiKey", visitorId: nil, config: FSConfig(.BUCKETING, timeout:0.4)) { (result) in
         
         
         /// When the sdk is ready ...
         if result == .Ready{
             
-//            
-//            Flagship.sharedInstance.activateModification(key: "complex")
-//            
-//            Flagship.sharedInstance.activateModification(key: "alias")
-//            
-//            Flagship.sharedInstance.activateModification(key: "array")
-//            
-//            let result = Flagship.sharedInstance.getModification("complex", defaultJson:[:])
-//            
-//
-//            let resultArray = Flagship.sharedInstance.getModification("array", defaultArray: [])
+            
+            Flagship.sharedInstance.activateModification(key: "complex")
+            
+            Flagship.sharedInstance.activateModification(key: "alias")
+            
+            Flagship.sharedInstance.activateModification(key: "array")
+            
+            let result = Flagship.sharedInstance.getModification("complex", defaultJson:[:])
+            
+
+            let resultArray = Flagship.sharedInstance.getModification("array", defaultArray: [])
 //            
 
 
