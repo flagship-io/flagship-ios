f740858cb895c5c78a02cfe0f8fddb1c0863961d
ABTastyAdel <adel@abtasty.com>
2022-11-18 18:59:26 +0100

    use systeminfo to get device model

--- FlagShip/Source/Audience/FSDevice.swift
+++ FlagShip/Source/Audience/FSDevice.swift
@@ -16,15 +16,15 @@ import Foundation
 
 
 internal class FSDevice: NSObject {
-
+    
     class func getDeviceLanguage() -> String? {
-
+        
         return NSLocale.current.languageCode
     }
-
+    
     /// Type od device
     class func getDeviceType() -> String {
-        #if os(iOS) || os(tvOS)
+#if os(iOS) || os(tvOS)
         switch UIDevice.current.userInterfaceIdiom {
         case .phone:
             return "Mobile"
@@ -35,48 +35,58 @@ internal class FSDevice: NSObject {
         default:
             return "Mobile"
         }
-        #elseif os(macOS)
-            return "Desktop"
-        #elseif os(watchOS)
+#elseif os(macOS)
+        return "Desktop"
+#elseif os(watchOS)
         return "Watch"
-        #else
+#else
         return ""
-        #endif
+#endif
     }
     
-
-      
-
+    
+    
+    
     
     
     /// Get the Model
     class func getDeviceModel() -> String {
+#if os(iOS)
         var systemInfo = utsname()
         uname(&systemInfo)
         let machineMirror = Mirror(reflecting: systemInfo.machine)
         return machineMirror.children.reduce("") { identifier, element in
-          guard let value = element.value as? Int8, value != 0 else { return identifier }
-          return identifier + String(UnicodeScalar(UInt8(value)))
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
         }
+#elseif os(tvOS)
+        return UIDevice.current.model
+#elseif os (macOS)
+        return FSDevice.getModelIdentifier() ?? "Mac"
+#elseif os(watchOS)
+        return  WKInterfaceDevice.current().model
+#else
+        return ""
+#endif
     }
     
     class func isFirstTimeUser() -> Bool {
-
+        
         let startedBefore = UserDefaults.standard.bool(forKey: "sdk_firstTimeUser")
         if startedBefore {
-          
+            
         } else {
             UserDefaults.standard.set(true, forKey: "sdk_firstTimeUser")
         }
         // FIRST_TIME_USER : TRUE (FALSE if the user is a returning one)
         return !startedBefore
     }
-
+    
     class func validateIpAddress(ipToValidate: String) -> Bool {
-
+        
         var sin = sockaddr_in()
         var sin6 = sockaddr_in6()
-
+        
         if ipToValidate.withCString({ cstring in inet_pton(AF_INET6, cstring, &sin6.sin6_addr) }) == 1 {
             // IPv6 peer.
             return true
@@ -84,33 +94,33 @@ internal class FSDevice: NSObject {
             // IPv4 peer.
             return true
         }
-
+        
         return false
     }
     
     
     class func getSystemVersion()->String{
-        #if os(iOS) || os(tvOS)
+#if os(iOS) || os(tvOS)
         return UIDevice.current.systemVersion
-        #elseif os(macOS)
+#elseif os(macOS)
         return FSDevice.getOSversion()
-        #elseif os(watchOS)
+#elseif os(watchOS)
         WKInterfaceDevice.current().systemVersion
-        #else
+#else
         return ""
-        #endif
+#endif
     }
     
     class func getSystemVersionName()->String{
-        #if os(iOS) || os(tvOS)
+#if os(iOS) || os(tvOS)
         return UIDevice.current.systemName
-        #elseif os(macOS)
+#elseif os(macOS)
         return FSDevice.getOSversionName()
-        #elseif os(watchOS)
+#elseif os(watchOS)
         WKInterfaceDevice.current().systemName
-        #else
+#else
         return ""
-        #endif
+#endif
     }
     
     
@@ -139,10 +149,10 @@ internal class FSDevice: NSObject {
         if let modelData = IORegistryEntryCreateCFProperty(service, "model" as CFString, kCFAllocatorDefault, 0).takeRetainedValue() as? Data {
             modelIdentifier = String(data: modelData, encoding: .utf8)?.trimmingCharacters(in: .controlCharacters)
         }
-
+        
         IOObjectRelease(service)
         return modelIdentifier
     }
 #endif
 }
- 
+
