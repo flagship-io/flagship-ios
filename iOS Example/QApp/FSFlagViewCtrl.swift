74085a34dcd28ea13f9cc9eab7a7878b5e138313
Adel <adel@abtasty.com>
2023-09-25 15:43:31 +0200

    clean code

--- iOS Example/QApp/FSFlagViewCtrl.swift
+++ iOS Example/QApp/FSFlagViewCtrl.swift
@@ -9,6 +9,14 @@
 import UIKit
 import Flagship
 
+
+internal enum FSValueType {
+    case DoubleType
+    case IntegerType
+    case StringType
+    case BooleanType
+}
+
 class FSFlagViewCtrl: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var currentFlag:FSFlag?
@@ -204,4 +212,31 @@ class FlagView:UIView, UIPickerViewDelegate, UIPickerViewDataSource{
         return true
     }
     
+}
+
+
+extension Data {
+    var prettyPrintedJSONString: NSString? { // NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
+
+class FSLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.borderWidth = 1
+        layer.borderColor = UIColor(red: 223/250, green: 68/250, blue: 110/250, alpha: 1).cgColor
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
