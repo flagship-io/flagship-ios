9605a5ee9c58bc9904ba6ae2166a65b07c24827b
Adel <adel@abtasty.com>
2024-04-19 10:49:13 +0200

    clean

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "Flagship", path: "FlagShip/Source", exclude: ["iOS Example"],)
+        .target(name: "Flagship", path: "FlagShip/Source", exclude: ["iOS Example"])
     ]
 )
