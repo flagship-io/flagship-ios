27f85c6a8b1b60c407e84c49ba29802c55b7aa55
Adel <adel@abtasty.com>
2024-05-29 18:38:49 +0200

    add log

--- FlagShip/Source/Core/FSFlag.swift
+++ FlagShip/Source/Core/FSFlag.swift
@@ -1,107 +1,127 @@
-////
-////  FSFlag.swift
-////  Flagship
-////
-////  Created by Adel on 23/02/2022.
-////
 //
+//  FlagBis.swift
+//  Flagship
+//
+//  Created by Adel Ferguen on 23/04/2024.
+//  Copyright © 2024 FlagShip. All rights reserved.
+//
+
 import Foundation
-//import Network
-//
-//public class FSFlag: NSObject {
-//    var key: String
-//    var defaultValue: Any?
-//    var strategy: FSStrategy?
-//    private var _status: FSFlagStatus {
-//        return strategy?.getStrategy().getFlagStatus(key) ?? .NOT_FOUND
-//    }
-//    
-//    public var status: FSFlagStatus {
-//        return _status
-// 
-//    } 
-// 
-//    
-//    init<T>(_ aKey: String, _ aModification: FSModification?, _ aDefaultValue: T? = nil, _ aStrategy: FSStrategy?) {
-//        key = aKey
-//        defaultValue = aDefaultValue
-//        strategy = aStrategy
-//    }
-//    
-//    @objc public func value(visitorExposed: Bool = true)->Any? {
-//        var result: Any?
-//        if let flagModification = strategy?.getStrategy().getFlagModification(key) {
-//            if isSameType_or_DefaultValue_Nil(flagModification.value) { /// _ have same type with default value OR the default value is nil
-//                ///
-//                FlagshipLogManager.Log(level: .ALL, tag: .GET_MODIFICATION, messageToDisplay: .MESSAGE("The value of the flag `\(key)` is `\(flagModification.value)"))
-//                
-//                result = flagModification.value
-//            } else {
-//                result = defaultValue
-//                FlagshipLogManager.Log(level: .ALL, tag: .ACTIVATE, messageToDisplay: FSLogMessage.MESSAGE("Return the default value due to the Type error"))
-//                
-//                // Send TR on not the same type flag
-//                FSDataUsageTracking.sharedInstance.proceesTSFlag(crticalPointLabel: .GET_FLAG_VALUE_TYPE_WARNING, f: self, v: strategy?.visitor)
-//                return defaultValue
-//            }
-//            
-//            /// Activate
-//            if visitorExposed {
-//                self.visitorExposed()
-//            }
-//            return result
-//        }
-//        FlagshipLogManager.Log(level: .ALL, tag: .GET_MODIFICATION, messageToDisplay: .MESSAGE("The key doon't exist ===> return the default value \(defaultValue ?? "")"))
-//        
-//        // Send TR on not found flag
-//        FSDataUsageTracking.sharedInstance.proceesTSFlag(crticalPointLabel: .GET_FLAG_VALUE_FLAG_NOT_FOUND, f: self, v: strategy?.visitor)
-//        return defaultValue
-//    }
-//    
-//    @available(*, deprecated, message: "Use visitorExposed()")
-//    @objc public func userExposed() {
-//        visitorExposed()
-//    }
-//    
-//    @objc public func visitorExposed() {
-//        if let flagModification = strategy?.getStrategy().getFlagModification(key) {
-//            /// The activate can be activated event whatever the type if the flag's value is nil
-//            if flagModification.value is NSNull || isSameType_or_DefaultValue_Nil(flagModification.value) {
-//                /// Activate the flag
-//                strategy?.getStrategy().activateFlag(self)
-//            } else {
-//                // Send TR on not the same type flag
-//                FSDataUsageTracking.sharedInstance.proceesTSFlag(crticalPointLabel: .GET_FLAG_VALUE_TYPE_WARNING, f: self, v: strategy?.visitor)
-//                FlagshipLogManager.Log(level: .ALL, tag: .ACTIVATE, messageToDisplay: .ACTIVATE_FAILED)
-//            }
-//        } else {
-//            FlagshipLogManager.Log(level: .ALL, tag: .ACTIVATE, messageToDisplay: FSLogMessage.MESSAGE("Return the default value due to the Type error"))
-//            // Send TR on flag not found
-//            FSDataUsageTracking.sharedInstance.proceesTSFlag(crticalPointLabel: .VISITOR_EXPOSED_FLAG_NOT_FOUND, f: self, v: strategy?.visitor)
-//        }
-//    }
-//    
-//    @objc public func exists()->Bool {
-//        return (strategy?.getStrategy().getModificationInfo(key) != nil)
-//    }
-//    
-//    @objc public func metadata()->FSFlagMetadata {
-//        if let flagModification = strategy?.getStrategy().getFlagModification(key) {
-//            if flagModification.value is NSNull || isSameType_or_DefaultValue_Nil(flagModification.value) {
-//                return FSFlagMetadata(flagModification)
-//            }
-//        }
-//        return FSFlagMetadata(nil)
-//    }
-//    
-//    /// _ Check the type of flag's value with the default value
-//    /// _ This check return true when the default value is nil
-//    private func isSameType_or_DefaultValue_Nil<T>(_ value: T)->Bool {
-//        var matchedType = false
-//        /// If the default value given in the getFalg() is nil then the check return true
-//        if defaultValue == nil {
-//            return true
-//        }
+
+public class FSFlag: NSObject {
+    // Key for flag modification
+    var key: String
+    // Strategy to stand for in any action
+    var strategy: FSStrategy?
+    // Safer exposure to display warning
+    private var isSafeToExpose: Bool = false
+    // Default value changing on the fly
+    public private(set) var defaultValue: Any? /// Change on the fly
+    // Current status
+    private var _status: FSFlagStatus {
+        return strategy?.getStrategy().getFlagStatus(key) ?? .NOT_FOUND
+    }
+
+    // Status value
+    public var status: FSFlagStatus {
+        return _status
+    }
+    
+    // Init without a defaultValue
+    init(_ aKey: String, _ aStrategy: FSStrategy?) {
+        key = aKey
+        strategy = aStrategy
+    }
+    
+    /// Getting value for flag
+    /// - Parameters:
+    ///   - defaultValue: input given by the developer
+    ///   - visitorExposed: optional input to expose flag
+    /// - Returns: Return a flag value. See the documentation for more details
+ 
+    public func value<T>(defaultValue: T?, visitorExposed: Bool = true)->T? {
+        var result: T?
+        // Update the default value
+        self.defaultValue = defaultValue
+        if let flagModification = strategy?.getStrategy().getFlagModification(key) {
+            if isSameType_or_DefaultValue_Nil(defaultValue, flagModification.value) { /// _ have same type with default value OR the default value is nil
+                ///
+                FlagshipLogManager.Log(level: .ALL, tag: .GET_MODIFICATION, messageToDisplay: .MESSAGE("The value of the flag `\(key)` is `\(flagModification.value)"))
+                
+                result = flagModification.value as? T
+            } else {
+                result = defaultValue
+                let msg = "For the visitor \(strategy?.visitor.visitorId), no flags were found with the key \(key). Therefore, the default value \(defaultValue) has been returned"
+                FlagshipLogManager.Log(level: .ALL, tag: .ACTIVATE, messageToDisplay: FSLogMessage.MESSAGE(msg))
+            }
+
+            /// Activate
+            if visitorExposed {
+                self.visitorExposed()
+            }
+            return result
+        }
+        FlagshipLogManager.Log(level: .ALL, tag: .GET_MODIFICATION, messageToDisplay: .MESSAGE("The key doon't exist ===> return the defaultValue"))
+        
+        return defaultValue
+    }
+    
+    #warning("Impact with TRoubleshooting, Need to adapt ")
+    @objc public func visitorExposed() {
+        /// check if the value function is called before
+        
+        if let flagModification = strategy?.getStrategy().getFlagModification(key) {
+            // Before activate we should
+            // - Check if the value() is called
+            // To display message warning
+            isSaferExposure()
+            
+            strategy?.getStrategy().activateFlag(self)
+            
+        } else {
+            FlagshipLogManager.Log(level: .ALL, tag: .ACTIVATE, messageToDisplay: FSLogMessage.MESSAGE("For the visitor \(strategy?.visitor.visitorId ?? ""), no flags were found with the key \(key). As a result, user exposure will not be sent."))
+            // Send TR on flag not found
+            FSDataUsageTracking.sharedInstance.proceesTSFlag(crticalPointLabel: .VISITOR_EXPOSED_FLAG_NOT_FOUND, f: self, v: strategy?.visitor)
+        }
+    }
+    
+    private func isSaferExposure() {
+        if isSafeToExpose {
+            print("It okay to expose the \(key) flag, all conditions seems to be correct ")
+        } else {
+            if defaultValue == nil {
+                print("Is not recommended to expose  \(key) flag since the defaultValue is not provided ")
+            } else {
+                print("Is not recommended to expose  \(key) flag since the defaultValue provided conflict with the value")
+            }
+        }
+
+        // return isSafeToExpose
+    }
+ 
+    @objc public func exists()->Bool {
+        return (strategy?.getStrategy().getModificationInfo(key) != nil)
+    }
+    
+    @objc public func metadata()->FSFlagMetadata {
+        if let flagModification = strategy?.getStrategy().getFlagModification(key) {
+            return FSFlagMetadata(flagModification)
+        }
+        return FSFlagMetadata(nil)
+    }
+    
+    /// _ Check the type of flag's value with the default value
+    /// _ This check return true when the default value is nil
+    private func isSameType_or_DefaultValue_Nil<T>(_ defaultValue: T?, _ value: Any)->Bool {
+        var matchedType = false
+        /// If the default value given in the getFalg() is nil then the check return true
+        if defaultValue == nil {
+            return true
+        }
+        matchedType = value is T; isSafeToExpose = matchedType
+        
+        return matchedType
+        
 //        switch defaultValue {
 //        case _ as String:
 //            /// Compare with String
@@ -112,6 +132,9 @@ import Foundation
 //        case _ as Bool:
 //            /// Compare with Boolean
 //            matchedType = value is Bool
+//        case _ as Float:
+//            /// Compare with Float
+//            matchedType = value is Float
 //        case _ as Double:
 //            /// Compare with Double
 //            matchedType = value is Double
@@ -124,81 +147,9 @@ import Foundation
 //        default:
 //            matchedType = false
 //        }
-//        
-//        return matchedType
-//    }
-//}
-//
-@objc public class FSFlagMetadata: NSObject {
-    public private(set) var campaignId: String = ""
-    public private(set) var variationGroupId: String = ""
-    public private(set) var variationId: String = ""
-    public private(set) var isReference: Bool = false
-    public private(set) var campaignType: String = ""
-    public private(set) var slug: String = ""
-    public private(set) var campaignName: String = ""
-    public private(set) var variationGroupName: String = ""
-    public private(set) var variationName: String = ""
-
-    init(_ modification: FSModification?) {
-        campaignId = modification?.campaignId ?? ""
-        variationGroupId = modification?.variationGroupId ?? ""
-        variationId = modification?.variationId ?? ""
-        isReference = modification?.isReference ?? false
-        campaignType = modification?.type ?? ""
-        slug = modification?.slug ?? ""
-        campaignName = modification?.campaignName ?? ""
-        variationGroupName = modification?.variationGroupName ?? ""
-        variationName = modification?.variationName ?? ""
-    }
-    
-    @objc public func toJson()->[String: Any] {
-        return ["campaignId": campaignId,
-                "campaignName": campaignName,
-                "variationGroupId": variationGroupId,
-                "variationGroupName": variationGroupName,
-                "variationId": variationId,
-                "variationName": variationName,
-                "isReference": isReference,
-                "campaignType": campaignType,
-                "slug": slug]
-    }
-}
-//
-/**
- * This status represent the flag status depend on visitor actions
- */
-@objc enum FlagSynchStatus: Int {
-    // When visitor is created
-    case CREATED
-    // When visitor context is updated
-    case CONTEXT_UPDATED
-    // When visitor Fetched flags
-    case FLAGS_FETCHED
-    // When visitor is authenticated
-    case AUTHENTICATED
-    // When visitor is unauthorised
-    case UNAUTHENTICATED
-    
-    /**
-      Return the string for the flag warning message.
-      Note: No message for FLAGS_FETCHED state
-     */
-    func warningMessage(_ flagKey: String, _ visitorId: String)->String {
-        var ret = ""
-        switch self {
-        case .CREATED:
-            ret = "Visitor `\(visitorId)` has been created without calling `fetchFlags` method afterwards, the value of the flag `\(flagKey)` may be outdated."
-        case .CONTEXT_UPDATED:
-            ret = "Visitor context for visitor `\(visitorId)` has been updated without calling `fetchFlags` method afterwards, the value of the flag `\(flagKey)` may be outdated."
-        case .AUTHENTICATED:
-            ret = "Visitor `\(visitorId)` has been authenticated without calling `fetchFlags` method afterwards, the value of the flag `\(flagKey)` may be outdated."
-        case .UNAUTHENTICATED:
-            ret = "Visitor `\(visitorId)` has been unauthenticated without calling `fetchFlags` method afterwards, the value of the flag `\(flagKey)` may be outdated."
-        default:
-            break
-        }
         
-        return ret
+        // update is safer exposure
+//        isSafeToExpose = matchedType
+//        return matchedType
     }
 }
--- FlagShip/Source/Core/FSVisitor+Flags.swift
+++ FlagShip/Source/Core/FSVisitor+Flags.swift
@@ -125,22 +125,5 @@ class FSExtraMetadata: FSFlagMetadata {
         // Add hex value
         ret.merge(["hex": self.hex]) { _, new in new }
         return ret
-
-//        do {
-//            guard let object = try? JSONSerialization.data(withJSONObject: ret) else {
-//                return ""
-//            }
-//            return NSString(data: object, encoding: String.Encoding.utf8.rawValue) ?? ""
-//        }
-    }
-}
-
-extension Data {
-    var prettyPrintedJSONStringB: NSString? { /// NSString gives us a nice sanitized debugDescription
-        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
-              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
-              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
-
-        return prettyPrintedString
     }
 }
