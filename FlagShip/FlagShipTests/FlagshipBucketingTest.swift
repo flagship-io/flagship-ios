15f6c5697ba938f50398112d51d7025dfdd48655
Adel <adel@abtasty.com>
2024-07-08 12:02:14 +0200

    fix test

--- FlagShip/FlagShipTests/FlagshipBucketingTest.swift
+++ FlagShip/FlagShipTests/FlagshipBucketingTest.swift
@@ -60,7 +60,7 @@ class FlagshipBucketingTest: XCTestCase {
         
         // Set fake session
         if let aUrlFakeSession = urlFakeSession {
-            testVisitor?.configManager.decisionManager?.networkService.serviceSession = aUrlFakeSession
+            //    testVisitor?.configManager.decisionManager?.networkService.serviceSession = aUrlFakeSession
         }
         
         testVisitor?.fetchFlags {
