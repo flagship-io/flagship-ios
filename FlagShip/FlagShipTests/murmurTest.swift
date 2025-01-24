1ca9e20a422364365e13c4c0fee8dcc809198fc1
Adel <adel@abtasty.com>
2025-01-24 16:38:46 +0100

    clean code

--- FlagShip/FlagShipTests/murmurTest.swift
+++ FlagShip/FlagShipTests/murmurTest.swift
@@ -86,12 +86,7 @@ class MurmurTest: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+ 
 
     func testHash() {
 
--- iOS Example/SmartQATests/SmartQATests.swift
+++ iOS Example/SmartQATests/SmartQATests.swift
@@ -6,11 +6,10 @@
 //  Copyright © 2023 FlagShip. All rights reserved.
 //
 
-import XCTest
 @testable import SmartQA
+import XCTest
 
 final class SmartQATests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -26,12 +25,4 @@ final class SmartQATests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
