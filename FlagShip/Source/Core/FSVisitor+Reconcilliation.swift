50276fe3a26e91839071c7ac482de79d875269b5
Adel <adel@abtasty.com>
2025-06-16 11:57:33 +0200

    update context when xpc

--- FlagShip/Source/Core/FSVisitor+Reconcilliation.swift
+++ FlagShip/Source/Core/FSVisitor+Reconcilliation.swift
@@ -15,15 +15,22 @@ public extension FSVisitor {
     /// - Important: After using this method, you should use Flagship.fetchFlags method to update the visitor informations
     /// - Requires: Make sure that the experience continuity option is enabled on the flagship platform before using this method
     @objc func authenticate(visitorId: String) {
+        if configManager.flagshipConfig.mode != .DECISION_API {
+            FlagshipLogManager.Log(level: .ALL, tag: .AUTHENTICATE, messageToDisplay: FSLogMessage.IGNORE_AUTHENTICATE)
+            return
+        }
         self.strategy?.getStrategy().authenticateVisitor(visitorId: visitorId)
         self.updateStateAndTriggerCallback(true)
         // Troubleshooting xpc
         FSDataUsageTracking.sharedInstance.processTSXPC(label: CriticalPoints.VISITOR_AUTHENTICATE.rawValue, visitor: self)
-
     }
 
     /// Use authenticate methode to go from Logged in  session to logged out session
     @objc func unauthenticate() {
+        if configManager.flagshipConfig.mode != .DECISION_API {
+            FlagshipLogManager.Log(level: .ALL, tag: .UNAUTHENTICATE, messageToDisplay: FSLogMessage.IGNORE_AUTHENTICATE)
+            return
+        }
         self.strategy?.getStrategy().unAuthenticateVisitor()
         self.updateStateAndTriggerCallback(false)
         // Troubleshooting xpc
