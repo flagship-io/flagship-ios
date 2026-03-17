0600c3799042d5492e7683a2ab161e25437dabce
Adel <adel@abtasty.com>
2026-03-17 12:50:55 +0100

    clean unit test

--- FlagShip/FlagShipTests/FlagshipTests.swift
+++ FlagShip/FlagShipTests/FlagshipTests.swift
@@ -11,48 +11,48 @@ import XCTest
 
 class FlagshipTests: XCTestCase {
     override func setUpWithError() throws {}
-    
+
     func testStart() {
         Flagship.sharedInstance.start(envId: "gk87t3jggr10c6l6sdob", apiKey: "apiKey")
         XCTAssert(Flagship.sharedInstance.envId == "gk87t3jggr10c6l6sdob")
         XCTAssert(Flagship.sharedInstance.apiKey == "apiKey")
         XCTAssert(Flagship.sharedInstance.currentStatus == .SDK_INITIALIZED)
-        
+
         XCTAssert(Flagship.sharedInstance.currentConfig.logLevel == .ALL)
         XCTAssert(Flagship.sharedInstance.currentConfig.mode == .DECISION_API)
         XCTAssert(Flagship.sharedInstance.currentConfig.timeout == 2)
     }
-    
+
     func testStartWithConfig() {
         let fsConfig = FSConfigBuilder().DecisionApi().withTimeout(12).build()
         Flagship.sharedInstance.start(envId: "gk87t3jggr10c6l6sdob", apiKey: "apiKey", config: fsConfig)
         XCTAssert(Flagship.sharedInstance.envId == "gk87t3jggr10c6l6sdob")
         XCTAssert(Flagship.sharedInstance.apiKey == "apiKey")
         XCTAssert(Flagship.sharedInstance.currentStatus == .SDK_INITIALIZED)
-        
+
         XCTAssert(Flagship.sharedInstance.currentConfig.logLevel == .ALL)
         XCTAssert(Flagship.sharedInstance.currentConfig.mode == .DECISION_API)
         XCTAssert(Flagship.sharedInstance.currentConfig.timeout == 12 / 1000)
     }
-    
+
     func testLogManager() {
         let customLoger = FSLogManager()
         customLoger.level = .WARNING
         customLoger.onLog(level: .DEBUG, tag: "testTag", message: "testMsg")
         XCTAssertTrue(customLoger.getLevel() == .WARNING)
     }
-    
+
     func testInitFromThread() {
         let expectation = XCTestExpectation(description: "Init Flagship from background thread completion handler")
-        
-        DispatchQueue.global(qos: .background).async {
+
+        DispatchQueue.global(qos: .userInitiated).async {
             if Flagship.sharedInstance.currentStatus == .SDK_NOT_INITIALIZED {
                 Flagship.sharedInstance.start(envId: "gk87t3jggr10c6l6sdob", apiKey: "apiKey")
                 XCTAssert(Flagship.sharedInstance.currentStatus == .SDK_INITIALIZED)
             }
             expectation.fulfill()
         }
-        
-        wait(for: [expectation], timeout: 5.0)
+
+        wait(for: [expectation], timeout: 10.0)
     }
 }
